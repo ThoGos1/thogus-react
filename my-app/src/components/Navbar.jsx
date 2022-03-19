2a61a65765f1eb4a29cd0b0c028654e7d602f285
ThoGos <34140015+ThoGos1@users.noreply.github.com>
2022-03-19 00:50:59 -0700

    ready for nav bar li-a overhaul

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -10,10 +10,17 @@ export default function Navbar() {
           paddingBottom: "1rem"
         }}
         >
-        <Link to="/" style={{margin: '1.5rem', fontWeight: 'bold', color: '#262626', fontSize: '30px', marginRight: '35%', marginTop: '1rem'}}>ThoGus</Link>
-        <Link to="/invoices" style={{ marginBottom: '3rem' }}>Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link> |{" "}
-        <Link to="/welcome">Welcome</Link>
+        <ul style={{marginTop: '4px', marginBottom: '-8px'}}>
+            <Link to="/" style={{margin: '1.5rem', fontWeight: 'bold', color: '#262626', fontSize: '30px', marginRight: '35%' }}>ThoGus</Link>
+            <Link to="/invoices" style={{ marginBottom: '3rem' }}>Invoices</Link> |{" "}
+            <Link to="/expenses">Expenses</Link> |{" "}
+            
+            <li>
+                <a>
+                    <Link to="/welcome">Welcome</Link>
+                </a>
+            </li>
+        </ul>
       </nav>
 
     </div>
